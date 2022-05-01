87f40c0914bab0665f1c1b4b950ca9a7770f02b4
Gyeyoung, Jung <gye0203@gmail.com>
2022-05-01 23:12:46 +0900

    2022-05-01

---
+++ Baekjoon_online_judge/Num_2338.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == '__main__':
+    a = int(sys.stdin.readline().strip('\n'))
+    b = int(sys.stdin.readline().strip('\n'))
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
